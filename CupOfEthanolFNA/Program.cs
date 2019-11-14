5674c1c763c5abceb6c416fb4aae74d32502658f
Eoin Flanagan <outterest@hotmail.com>
2019-11-14 13:26:34 +0100

    Now shows a special error message when opengl invalid enum error occurs

--- CupOfEthanolFNA/Program.cs
+++ CupOfEthanolFNA/Program.cs
@@ -7,28 +7,41 @@
         [STAThreadAttribute]
         private static void Main(string[] args)
         {
-            //try
-            //{
-                using (MainMethod game = new MainMethod(args))
-                {
-                    game.Run();
-                }
-            //}
-			//catch (Exception ex)
-			//{
-			//	ErrorReporter.LogException(new string[] {
-			//		"Game crashed",
-			//		"Message = " + ex.Message,
-			//		"MethodName = " + ex.TargetSite.Name,
-			//		ex.StackTrace,
-			//	});
-			//	System.Windows.Forms.MessageBox.Show(
-			//		"Please share the content of error.txt located in the installation folder\n" +
-			//		"You can get in touch by emailing flanagep" + "@t" + "cd" + ".ie\n\n" +
-			//		"I will try my best to fix the issue asap", 
-			//		"An unexpected error occured");
-			//	throw ex;
-			//}
+			try
+			{
+				using (MainMethod game = new MainMethod(args))
+				{
+					game.Run();
+				}
+			}
+			catch (Exception ex)
+			{
+				ErrorReporter.LogException(new string[] {
+					"Game crashed",
+					"Message = " + ex.Message,
+					"MethodName = " + ex.TargetSite.Name,
+					ex.StackTrace,
+				});
+				if (ex.Message.Contains("GL error GL_INVALID_ENUM in MatrixMode"))
+				{
+					System.Windows.Forms.MessageBox.Show(
+						"There was an error with OpenGL graphics API\n" +
+						"Here are some potential solutions:\n\n" +
+						"1. Disable steam overlay\n" +
+						"2. Update your graphics drivers\n" +
+						"3. Close steam and launch the game executable manually\n\n" +
+						"If none of these work, perhaps some other program is interfering with the game's graphics that you need to disable while the game is running",
+						"OpenGL error");
+				} else
+				{
+					System.Windows.Forms.MessageBox.Show(
+						"Please share the content of error.txt located in the installation folder\n" +
+						"You can get in touch by emailing flanagep" + "@t" + "cd" + ".ie\n\n" +
+						"I will try my best to fix the issue asap",
+						"An unexpected error occured");
+				}
+				throw ex;
+			}
 		}
 	}
 }
